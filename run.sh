3ebe8023a5d9accf3efbef07e0c4cbfd973a8bd0
bachsofttrick <xuanbach1307@gmail.com>
2026-06-04 21:29:24 -0700

    add BUILD_ACCOUNT from run.sh to env

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-ACCOUNT=phanx
 
-sudo -u $ACCOUNT ./build.sh 2>&1
+# Load from .env file
+if [ -f .env ]; then
+    export $(cat .env | grep -v '^#' | xargs)
+else
+    echo "Error: .env file not found"
+    exit 1
+fi
+
+sudo -u $BUILD_ACCOUNT ./build.sh 2>&1
